b37a7d8f6bfbdc7a0f02ef59dc7031c065af21d2
Anderson <daosantiago@gmail.com>
2023-05-23 15:43:56 +0100

    update wsgi

--- amazonia/amazonia/wsgi.py
+++ amazonia/amazonia/wsgi.py
@@ -14,5 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amazonia.settings')
 
 
-# application = get_wsgi_application()
-application = 'vercel_app.wsgi.app'
+application = get_wsgi_application()
+
+app = application
+# application = 'vercel_app.wsgi.app'
